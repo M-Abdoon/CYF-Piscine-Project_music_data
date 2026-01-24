5737393027625258361af21439ee81d2bc68344d
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2026-01-24 20:52:14 +0000

    filtering data

--- script.mjs
+++ script.mjs
@@ -10,11 +10,11 @@ function setup () {
 	const userSelectEl = document.getElementById("UserSelect");
 	const answer1El = document.getElementById("answer-1");
 	const answer2El = document.getElementById("answer-2");
-	const answer3El = document.getElementById("answer-2");
-	const answer4El = document.getElementById("answer-2");
-	const answer5El = document.getElementById("answer-2");
-	const answer6El = document.getElementById("answer-2");
-	const answer7El = document.getElementById("answer-2");
+	const answer3El = document.getElementById("answer-3");
+	const answer4El = document.getElementById("answer-4");
+	const answer5El = document.getElementById("answer-5");
+	const answer6El = document.getElementById("answer-6");
+	const answer7El = document.getElementById("answer-7");
 
 	const usersIds = getUserIDs();
 
@@ -27,26 +27,59 @@ function setup () {
 
 		answer1El.textContent = getSong(getMostListenedSong(selectedUser)).title;
 		answer2El.textContent = getSong(getMostListenedSong(selectedUser)).artist;
-		// answer3El.textContent = getSong(getMostListenedSong(selectedUser)).title;
+		answer3El.textContent = getSong(getMostListenedSong(selectedUser, "Friday", "17")).artist;
+		answer3El.textContent += ` - ${ getSong(getMostListenedSong(selectedUser, "Friday", "17")).title }`;
+
 		// answer4El.textContent = getSong(getMostListenedSong(selectedUser)).title;
 		// answer5El.textContent = getSong(getMostListenedSong(selectedUser)).title;
 		// answer6El.textContent = getSong(getMostListenedSong(selectedUser)).title;
 		// answer7El.textContent = getSong(getMostListenedSong(selectedUser)).title;
 
 		
-		console.log(getListenEvents(1));
-		console.log(timestampToDayName("2024-08-01T00:20:07"));
+		//console.log(timestampToDayName("2024-08-01T00:20:07"));
+		//console.log(new Date("2024-08-01T00:21:07").getHours());
+		//console.log(getListenEvents(1));
 
 	});
 }
+
+
 function timestampToDayName(timestamp) {
 	return new Date(timestamp).toLocaleDateString("en-UK", { weekday: "long"});
 }
 
-function getMostListenedSong(userId) {
-	const allUserSongs = getListenEvents(userId);
-	let counts = [];
+function timestampToHour(timestamp) {
+	return new Date(timestamp).getHours();
+}
 
+function getMostListenedSong(userId, dayWeek, hours) {
+	let allUserSongs = getListenEvents(userId);
+	let counts = [];
+	let countByDay = dayWeek? true : false;
+	let countByHour= hours? true : false;
+	let newSongsArray = [];
+	let i = 0;
+
+	if (countByDay === true && countByHour === true) {
+		
+		allUserSongs.forEach(song => {
+			if (timestampToDayName(song.timestamp) === dayWeek &&
+				timestampToHour(song.timestamp) >= hours)
+				console.log(`timestamp: ${timestampToHour(song.timestamp)} 
+				hours: ${hours}`);
+		});
+
+		// allUserSongs = allUserSongs.filter(song => 
+		// 	(timestampToDayName(song.timestamp) === dayWeek
+		// ));
+
+	}
+
+	// if (countByHour === true) {
+	// 	allUserSongs = allUserSongs.filter(song => timestampToHour(song.timestamp) === countByHour);
+	// }
+
+	console.log(allUserSongs);
 	allUserSongs.forEach(song => {
 		counts[song.song_id] = (counts[song.song_id] || 0) + 1;
 	})
